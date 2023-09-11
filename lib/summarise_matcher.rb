13ef1627d98ea15ba5b7c638ea915dbb25c13d23
Frankie Roberto <frankie@frankieroberto.com>
2023-09-11 14:27:49 +0100

    Refactor into modules
    This refactors the matchers and the helper into modules.

--- lib/summarise_matcher.rb
+++ lib/summarise_matcher.rb
@@ -1,59 +1,71 @@
-RSpec::Matchers.define :summarise do |expected|
-  match do |_actual|
-    if expected[:action]
-      key_html && value_html && value_match? && action_link && action_link[:href] == expected[:action][:href]
-    else
-      key_html && value_html && value_match?
+module GovukRspecHelpers
+  module SummariseMatcher
+
+    extend RSpec::Matchers::DSL
+
+    define :summarise do |expected|
+      match do |_actual|
+        if expected[:action]
+          key_html && value_html && value_match? && action_link && action_link[:href] == expected[:action][:href]
+        else
+          key_html && value_html && value_match?
+        end
+      end
+
+      failure_message do |actual|
+        if !key_html
+          "Could not find the key ‘#{expected[:key]}’ within\n\n #{actual}"
+        elsif !value_html
+          "Could not find a <dd class=\"govuk-summary-list__value\"> element within HTML: \n#{row_html.native.to_html}"
+        elsif value_text != expected[:value]
+          "Expected ‘#{expected[:key]}’ value to be ‘#{expected[:value]}’ but was ‘#{value_text}’"
+        elsif !action_link
+          "Could not find the link ‘#{expected[:action][:text]}’ within HTML: \n#{actions_html.native.to_html}"
+        else
+          "Expected link href to be #{expected[:action][:href]}, was #{action_link[:href]}"
+        end
+      end
+
+      def html
+        @html ||= Capybara::Node::Simple.new(actual.is_a?(String) ? actual : actual.html)
+      end
+
+      def key_html
+        @key_html ||= html.all('dt.govuk-summary-list__key', exact_text: expected[:key], normalize_ws: true).first
+      end
+
+      def row_html
+        @row_html ||= key_html.ancestor('.govuk-summary-list__row')
+      end
+
+      def actions_html
+        @actions_html ||= row_html.all('dd.govuk-summary-list__actions').first
+      end
+
+      def value_html
+        @value_html ||= row_html.all('dd.govuk-summary-list__value').first
+      end
+
+      def value_text
+        @value_text ||= value_html.text(normalize_ws: true)
+      end
+
+      def action_link
+        @action_link ||= actions_html.all(:link, exact_text: expected[:action][:text], normalize_ws: true).first
+      end
+
+      def value_match?
+        if expected[:value].is_a?(Regexp)
+          value_text.match?(expected[:value])
+        else
+          value_text == expected[:value]
+        end
+      end
     end
-  end
 
-  failure_message do |actual|
-    if !key_html
-      "Could not find the key ‘#{expected[:key]}’ within\n\n #{actual}"
-    elsif !value_html
-      "Could not find a <dd class=\"govuk-summary-list__value\"> element within HTML: \n#{row_html.native.to_html}"
-    elsif value_text != expected[:value]
-      "Expected ‘#{expected[:key]}’ value to be ‘#{expected[:value]}’ but was ‘#{value_text}’"
-    elsif !action_link
-      "Could not find the link ‘#{expected[:action][:text]}’ within HTML: \n#{actions_html.native.to_html}"
-    else
-      "Expected link href to be #{expected[:action][:href]}, was #{action_link[:href]}"
+    RSpec.configure do |rspec|
+      rspec.include self
     end
-  end
 
-  def html
-    @html ||= Capybara::Node::Simple.new(actual.is_a?(String) ? actual : actual.html)
-  end
-
-  def key_html
-    @key_html ||= html.all('dt.govuk-summary-list__key', exact_text: expected[:key], normalize_ws: true).first
-  end
-
-  def row_html
-    @row_html ||= key_html.ancestor('.govuk-summary-list__row')
-  end
-
-  def actions_html
-    @actions_html ||= row_html.all('dd.govuk-summary-list__actions').first
-  end
-
-  def value_html
-    @value_html ||= row_html.all('dd.govuk-summary-list__value').first
-  end
-
-  def value_text
-    @value_text ||= value_html.text(normalize_ws: true)
-  end
-
-  def action_link
-    @action_link ||= actions_html.all(:link, exact_text: expected[:action][:text], normalize_ws: true).first
-  end
-
-  def value_match?
-    if expected[:value].is_a?(Regexp)
-      value_text.match?(expected[:value])
-    else
-      value_text == expected[:value]
-    end
   end
 end
